2487254dc68079f8a0c7c8a73669ed6707443dc1
April Wormwood <mewtek@icloud.com>
2023-07-08 15:51:26 -0700

    Test bomb setup and wire defusal test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod solvers;
+
 fn main() {
-    print!("Hello, world!");
+    let wires = vec!["black", "white", "black"];
+    let bomb = solvers::Bomb {
+        serial: String::from("Z8CP06"),
+        car: false,
+        frq: false,
+        battery_count: 3,
+        strikes: 0,
+    };
+
+    solvers::defuse_wires(wires, bomb);
 }
