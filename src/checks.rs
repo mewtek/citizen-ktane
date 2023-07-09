3883e8fcd08d8b51a4f8a66484801a31667c1dd2
April Wormwood <mewtek@icloud.com>
2023-07-08 22:35:44 -0700

    Fix checks::last_digit_odd returning true on an even number

--- src/checks.rs
+++ src/checks.rs
@@ -1,11 +1,11 @@
 
-/// Checks to see if the last digit of the bomb's serial number is odd.
+/// Checks to see if the last digit of the bomb's serial number is odd
 pub fn last_digit_odd(serial: String) -> bool
 {
     let last_digit_string = serial.chars().last().unwrap().to_string();
     let last_digit: i32 = last_digit_string.parse::<i32>().unwrap_or(0);
 
-    if last_digit % 2 == 0
+    if last_digit % 2 != 0
     {
         return true;
     }
@@ -13,7 +13,7 @@ pub fn last_digit_odd(serial: String) -> bool
     return false;
 }
 
-/// Checks to see if the bomb's serial number has a vowel or more in it.
+/// Checks to see if the bomb's serial number has one or more vowels in it
 pub fn serial_has_vowels(serial: String) -> bool
 {
     let vowels: Vec<char> = vec!['a', 'e', 'i', 'o', 'u'];
